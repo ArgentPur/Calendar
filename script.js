25af6966f83fe8078ccd59d9e0d5b5bfc1cffd88
Zachary Lynn <zalynn25@hotmail.com>
2020-09-28 16:26:09 -0400

    update

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
 $(".saveBtn").on("click", function() {
-   var event= $(textarea.textContent) 
+   var event= $("<textarea>".textContent);
+   var time= 
+
+   localStorage.setItem()
+
 });
+
+
+function timeUpdate() {
+   var currentHour = moment().hours()
+
+   $(".col-sm-8").each(function() {
+
+   });
+};
+
 
