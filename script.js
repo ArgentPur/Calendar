000011557e9624759c88e27062e6b49a96d1108d
Zachary Lynn <zalynn25@hotmail.com>
2020-09-28 21:12:04 -0400

    again

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var currentHour = moment().hours()
-
+var date = moment().date()
 console.log(currentHour)
+console.log(date)
 
 alert("hi")
 
@@ -8,7 +9,7 @@ $(".saveBtn").on("click", function() {
    var event= $(this).siblings(".textValue").val();
    var time=  $(this).siblings("")
 
-   localStorage.setItem(event)
+   localStorage.setItem(event, JSON.stringify)
    console.log(event)
 });
 
@@ -16,9 +17,9 @@ $(".saveBtn").on("click", function() {
 function timeUpdate() {
    var currentHour = moment().hours()
 
-   $(".col-sm-8").each(function() {
+   $(".time").each(function() {
 
-      var block = $()
+      var block = $(this)
 
 
       if(block = currentHour) {
