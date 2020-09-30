efd896a7ba49d8aedf98c5b441a7ed893878b625
Zachary Lynn <zalynn25@hotmail.com>
2020-09-29 20:22:37 -0400

    another

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-var currentHour = moment().hours()
-var date = moment().date()
-console.log(currentHour)
-console.log(date)
+var currentHour = moment().hours();
+var date = moment().date();
+console.log(currentHour);
+console.log(date);
 
 
 
 $(".saveBtn").on("click", function() {
    var event= $(this).siblings(".textValue").val();
-   var time=  $(this).siblings("")
+   var time=  $(this).siblings(".hour").val();
 
-   localStorage.setItem(event, JSON.stringify)
-   console.log(event)
+   localStorage.setItem(event, time, JSON.stringify);
+   console.log(event);
 });
 
 
@@ -19,14 +19,16 @@ function timeUpdate() {
 
    $(".time-block").each(function() {
 
-      var block = $(this)
+      var block = $(".time-block").parseInt()
 
 
       if(block = currentHour) {
+         $(this).class(".present");
+      };
 
-         
-
-      }
+      if(block > currentHour) {
+         $(this).class(".future")
+      };
 
 
 
